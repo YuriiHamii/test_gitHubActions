8aedf99a1c5a19d58f67c1c2b7d138bed2ddfb30
Yurii Hamii <58522152+YuriiHamii@users.noreply.github.com>
2022-06-18 21:52:33 +0300

    Update application.py
    hos update

--- application.py
+++ application.py
@@ -193,7 +193,7 @@ def upload():
 
 if __name__ == "__main__":
     # app.debug = True  
-    application.run( host='127.0.0.1', port=80, debug=True )
+    application.run( host='0.0.0.0', port=80, debug=True )
                                      # port=5000
     
 
