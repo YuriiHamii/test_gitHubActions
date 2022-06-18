98448c7a4c27692e731a4d9b8162d2759153d242
Yurii Hamii <58522152+YuriiHamii@users.noreply.github.com>
2022-06-19 01:57:30 +0300

    Update application Version 1.0.1

--- application.py
+++ application.py
@@ -1,20 +1,67 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, session, redirect, url_for, abort
 
 
 application = Flask(__name__)
 
 
+application.config['SECRET_KEY'] = 'fdgdfgdfggf786hfg6hxg6h7f'
 
 
+menu = [{"name": "Downloading", "url": "install-flask"},
+        {"name": "First application", "url": "first-app"},
+        {"name": "Feedback", "url": "contact"}]
 
-@application.route("/index")
+
 @application.route("/")
 def index():
-    return "index"
+    return render_template('index.html', menu=menu)
 
 @application.route("/about")
 def about():
-    return "<h1> About website BlogDeploy </h1>"
+    return render_template('about.html', title="About website", menu=menu)
+
+
+@application.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        print(request.form)
+        print(request.form['username'])
+       
+        if len(request.form['username']) > 2:
+            flash('message sent successfully', category='success')
+        else:
+            flash('message sending error', category='error')
+            
+
+    return render_template('contact.html', title="Feedback", menu = menu)
+
+
+
+@application.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
+    return f"User profile: {username}"
+
+
+
+@application.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "selfedu" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
+@application.errorhandler(404)
+
+def pageNotFount(error):
+    return render_template('page404.html', title="Page not found", menu=menu), 404
+
 
 
 if __name__ == "__main__":
