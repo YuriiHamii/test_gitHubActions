ca5e1d208076287282c347afc5cc0b41792fc665
Yurii Hamii <58522152+YuriiHamii@users.noreply.github.com>
2022-06-18 21:27:47 +0300

    Update application.py
    add port

--- application.py
+++ application.py
@@ -193,7 +193,8 @@ def upload():
 
 if __name__ == "__main__":
     # app.debug = True  
-    application.run( host='127.0.0.1', port=5000, debug=True )
+    application.run( host='127.0.0.1', port=80, debug=True )
+                                     # port=5000
     
 
 
