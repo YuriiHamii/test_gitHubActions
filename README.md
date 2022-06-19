a2d38ef41532bbbd51836738c7c6c028c76f05dd
Yurii Hamii <autocadmdav@gmail.com>
2022-06-19 13:30:47 +0300

    Add DB version 1.0.0

--- README.md
+++
@@ -1,7 +0,0 @@
-# test_gitHubActions 
-GitHub Actions - blogdeploy.tk
-
-Status of last deployment:<br>
-<img src="https://github.com/YuriiHamii/test_gitHubActions/workflows/My-GitHubActions-Basics/badge.svg?branch=main">
-          
-          
--- application.py
+++ application.py
@@ -1,69 +1,90 @@
-from flask import Flask, render_template, request, flash, session, redirect, url_for, abort
+import sqlite3
+import os
+from flask import Flask, render_template, request, flash, session, redirect, url_for, abort, g
+from FDataBase import FDataBase
 
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
 
 application = Flask(__name__)
 
 
-application.config['SECRET_KEY'] = 'fdgdfgdfggf786hfg6hxg6h7f'
+application.config.from_object(__name__)
+application.config.update(dict(DATABASE=os.path.join(application.root_path, 'flsite.db')))
+
+def connect_db():
+    conn = sqlite3.connect(application.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_db():
+    """Вспомогательная функция для создания таблиц БД"""
+    db = connect_db()
+    with application.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
 
 
-menu = [{"name": "Downloading", "url": "install-flask"},
-        {"name": "First application", "url": "first-app"},
-        {"name": "Feedback", "url": "contact"}]
+def get_db():
+    '''Start - Connection with DB'''
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+
+@application.teardown_appcontext
+def close_db(error):
+    '''Close - Connection with DB'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
 
 
 @application.route("/")
 def index():
-    return render_template('index.html', menu=menu)
-
-@application.route("/about")
-def about():
-    return render_template('about.html', title="About website", menu=menu)
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('index.html', menu = dbase.getMenu(), posts=dbase.getPostsAnonce())
 
 
-@application.route("/contact", methods=["POST", "GET"])
-def contact():
-    if request.method == 'POST':
-        print(request.form)
-        print(request.form['username'])
-       
-        if len(request.form['username']) > 2:
-            flash('message sent successfully', category='success')
+@application.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('ERROR  of add post', category='error')
+            else:
+                flash('Post added successfully', category='success')
         else:
-            flash('message sending error', category='error')
-            
+            flash('ERROR  of add post', category='error')
 
-    return render_template('contact.html', title="Feedback", menu = menu)
-
+    return render_template('addpost.html', menu=dbase.getMenu(), title="Add post")
 
 
-@application.route("/profile/<username>")
-def profile(username):
-    if 'userLogged' not in session or session['userLogged'] != username:
-        abort(401)
+@application.route("/post/<int:id_post>")
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
 
-    return f"User profile: {username}"
-
-
-
-@application.route("/login", methods=["POST", "GET"])
-def login():
-    if 'userLogged' in session:
-        return redirect(url_for('profile', username=session['userLogged']))
-    elif request.method == 'POST' and request.form['username'] == "selfedu" and request.form['psw'] == "123":
-        session['userLogged'] = request.form['username']
-        return redirect(url_for('profile', username=session['userLogged']))
-
-    return render_template('login.html', title="Авторизация", menu=menu)
-
-
-@application.errorhandler(404)
-
-def pageNotFount(error):
-    return render_template('page404.html', title="Page not found", menu=menu), 404
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
 
 
 if __name__ == "__main__":
-    application.debug = True
-    application.run()
+    application.run(debug=True)
+
+
+
+
+# Test
+# http://127.0.0.1:5000/add_post - 200 -  "GET /add_post HTTP/1.1"
+#                      /add_post - 200 =       /add_post
